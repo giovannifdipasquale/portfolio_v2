da590dff28d29ad07f8a97f2fad8490d3d06792d
giovannifdipasquale <giovanni.francesco97@gmail.com>
2026-01-19 20:29:04 +0100

    Installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <div>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
